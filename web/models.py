ad64d6fefd3832a7c362d9269a1a36e26d7154a2
amir <amirhaghparast412@gmail.com>
2020-10-28 00:58:19 +0330

    expense model is there

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    
+    
+
