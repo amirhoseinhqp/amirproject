12cbd7461347da6873d824fd35402b378768e2d8
amir <amirhaghparast412@gmail.com>
2020-10-28 23:15:22 +0330

    initaia commit

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import expense ,income
 
 # Register your models here.
+
+admin.site.register(expense)
+admin.site.register(income)
--- web/models.py
+++ web/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)     
+class income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     
-    
-
