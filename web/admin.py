b87d37136c4b1749ae13681f87ead34c41ff7e84
amir <amirhaghparast412@gmail.com>
2020-10-30 13:31:10 +0330

    initial commit

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import expense ,income
-
+from .models import expense,income
 # Register your models here.
 
 admin.site.register(expense)
--- web/models.py
+++ web/models.py
@@ -1,15 +1,21 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
+
 # Create your models here.
 
 class expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)     
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __unicode__(self):
+        return "{}-{}".format(self.date , self.amount)
+
 class income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __unicode__(self):
+        return "{}-{}".format(self.date , self.amount)
